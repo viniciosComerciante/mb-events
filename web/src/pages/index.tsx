011784d30ce2cacf727a829a9430d83f20684da6
Vinícios Oliveira <vinicompif@gmail.com>
2023-04-13 00:08:23 -0300

    remove useless files

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { Open_Sans } from 'next/font/google'
+
+const openSans = Open_Sans({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    </main>
+  )
+}
