dfc69b9e9873eef6e34855f343cc93c27d5a22ca
Vinícios Oliveira <vinicompif@gmail.com>
2023-04-14 00:27:45 -0300

    adjust searchbox text

--- web/src/components/header/searchBox/index.tsx
+++ web/src/components/header/searchBox/index.tsx
@@ -6,7 +6,7 @@ export function SearchBox() {
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
           </button>
         </span>
-        <input type="text" name="q" className="py-2 text-sm ease-in-out duration-100 text-white bg-primary-700 rounded-md pl-10 focus:outline-none placeholder- focus:bg-white focus:text-gray-900 w-full" placeholder="Pesquisar eventos acadêmicos, empresariais..."/>
+        <input type="text"  className="py-2 text-sm ease-in-out duration-100 text-white bg-primary-700 rounded-md pl-10 focus:outline-none placeholder- focus:bg-white focus:text-gray-900 w-full" placeholder="Pesquisar eventos acadêmicos, empresariais..."/>
       </div>
   );
 }
