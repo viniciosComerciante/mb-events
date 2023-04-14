7147f5aeff09de64619fd663506de4e2be6ee00a
Vinícios Oliveira <vinicompif@gmail.com>
2023-04-14 00:25:56 -0300

    create mocked database

---
+++ web/src/database.ts
@@ -0,0 +1,49 @@
+const db = {
+  categories:[ 
+    {
+      title: "Palestras",
+      img: "https://www.mm7palestras.com.br/images/palestrantes/Joel_Jota.jpeg",
+      id:1
+    },
+    {
+      title: "Webinários",
+      img: "https://www.gov.br/capes/pt-br/assuntos/noticias/qualidade-e-regulacao-na-ead-e-tema-de-seminario/07102019-bannerdestaque-seminario-jpg/@@images/7321b030-73ce-4439-b0f7-4ab889d6cc5b.jpeg",
+      id:2
+    },
+    {
+      title: "Startups",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAYVzfnjBwih_FdKZ1un8EaJhNNK6dPyXBYF1EwN-c1krfT0MwYTbKvl2zXfhkhLvn2nw&usqp=CAU",
+      id:3
+    },
+    {
+      title: "Aulas",
+      img: "https://www.gov.br/capes/pt-br/media/imagem_dentro/22112021_imagemdentro_uema.jpg",
+      id:4
+    },
+    {
+      title: "Feiras",
+      img: "https://pbs.twimg.com/profile_images/1575214895729201152/sf-Eaddp_400x400.jpg",
+      id:5
+    },
+    {
+      title: "Empreendedorismo",
+      img: "https://s2.glbimg.com/AN8ErvFR1PQriUdLm2FRTHepT5I=/400x400/top/e.glbimg.com/og/ed/f/original/2014/09/02/carreira-01.jpg",
+      id:6
+    },
+    {
+      title: "Pós graduação",
+      img: "https://edu.facsp.com.br/wp-content/uploads/2022/02/Pos_FAC-18-400x400.jpg",
+      id:7
+    },
+    {
+      title: "Workshops",
+      img: "https://consultoriaprimeirainfancia.com/wp-content/uploads/2019/07/wordkshop-2019-01.jpg",
+      id:8
+    }
+  ]
+}
+
+ 
+
+
+export default db;
